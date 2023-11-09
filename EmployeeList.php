bc40b5a3568d1da6f4d550383c60b0b7bf03b9a4
NeroXerith <astar8820@gmail.com>
2023-11-10 01:51:22 +0800

    changed title

--- EmployeeList.php
+++ EmployeeList.php
@@ -56,7 +56,7 @@ else{
 <html lang="en" dir="ltr">
   <head>
     <meta charset="utf-8">
-    <title>EMPLOYEE LIST</title>
+    <title>EMPLOYEE LIST - Activity</title>
 	  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
 	<link rel="stylesheet" href="https://cdn.datatables.net/1.13.6/css/jquery.dataTables.css" />
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
